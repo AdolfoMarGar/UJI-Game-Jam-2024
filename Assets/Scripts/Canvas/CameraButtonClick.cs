d10706414308b84a5071d6ec46deb4fefbbd654d
Artey-git <arteygs@gmail.com>
2024-10-26 13:16:47 +0200

    camera_rotation

--- Assets/Scripts/Canvas/CameraButtonClick.cs
+++ Assets/Scripts/Canvas/CameraButtonClick.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraButtonClick : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject cam;
+    [SerializeField] private GameObject click_i;
+    [SerializeField] private GameObject click_d;
+
+    // Movimiento camara
+    [SerializeField] private Vector3 cameraRotation;
+    [SerializeField] private float r_time;
+    [SerializeField] private bool rotation = false;
+
+    private Quaternion targetRotation;
+
     void Start()
     {
-        
+        targetRotation = Quaternion.Euler(cameraRotation);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        if (rotation) 
+        {
+            cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, targetRotation, Time.deltaTime / r_time);
+
+            if (Quaternion.Angle(cam.transform.rotation, targetRotation) < 0.01f) 
+            {
+                cam.transform.rotation = targetRotation;
+                rotation = false; 
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        rotation = true;
     }
 }
